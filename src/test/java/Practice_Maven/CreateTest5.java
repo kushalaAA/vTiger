38f84bd23f2855ce3d0cae194b7fc3fc769bb3d1
Me <Me@DESKTOP-HM27A8M>
2023-01-19 17:22:22 +0530

    commited test 5

---
+++ src/test/java/Practice_Maven/CreateTest5.java
@@ -0,0 +1,11 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class CreateTest5 {
+	@Test
+	public void test() {
+		System.out.println("---test created sucessfully----");
+	}
+
+}
